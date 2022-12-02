5676a0ea8acdf2187513c9fc0a52f164882cd81f
Aleix Carles <aleixcarles@iesebre.com>
2022-12-02 17:03:22 +0100

    Video Laravel Blade companents i Layouts finalitzat

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,6 +1,10 @@
-<p>{{ $video->title }}</p>
-<ul>
-    <li>Description: {{$video->description}}</li>
-    <li>Data: {{$video->published_at->format('F d')}}</li>
-</ul>
-<p>Ubuntu 101</p>
+<x-casteaching-layout>
+    <p>{{ $video->title }}</p>
+    <ul>
+        <li>Description: {{$video->description}}</li>
+        <li>Data: {{$video->published_at->format('F d')}}</li>
+    </ul>
+    <p>Ubuntu 101</p>
+</x-casteaching-layout>
+
+
