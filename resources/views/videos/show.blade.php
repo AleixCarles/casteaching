56cdebcaaf1462a37b33db9a8926cead926c2412
AleixCarles <aleixcarles@iesebre.com>
2023-02-15 18:54:33 +0100

    Video 109 aplicar estils des de zero Criteris de disseny TailwindCSS i Laravel mix finalitzat

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,10 +1,18 @@
 <x-casteaching-layout>
-    <p>{{ $video->title }}</p>
-    <ul>
-        <li>Description: {{$video->description}}</li>
-        <li>Data: {{$video->published_at->format('F d')}}</li>
-    </ul>
-    <p>Ubuntu 101</p>
+    <div class="flex flex-col h-screen">
+        <iframe
+                class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10 h-4/5"
+                src="https://www.youtube.com/embed/btGr3mPK1dU"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen></iframe>
+        <div class="p-4 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10">
+           {{ $video->title }}
+        </div>
+        <div class="p-4 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10">
+           {{$video->description}}
+        </div>
+    </div>
 </x-casteaching-layout>
 
 
