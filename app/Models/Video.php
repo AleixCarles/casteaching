50b7af400fff07f47abec31fa87a225069cea349
AleixCarles <aleixcarles@iesebre.com>
2023-02-23 17:57:43 +0100

    Video 112b min 2:40

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,4 +12,12 @@ class Video extends Model
     protected $guarded = [];
     protected $dates = ['published_at'];
 //    protected $casts = ['published_at'=>'datetime:Y-m-d'];
+
+//formatted_published_at accesor
+    public function getFormattedPublishedAtAttribute()
+    {
+//        dd($this->published_at->format('j \d\e F \d\e\ Y'));
+        $locale_date=$this->published_at->locale('ca_es');
+        return $locale_date->day . ' de ' . $locale_date->monthName . ' de ' . $locale_date->year;
+    }
 }
