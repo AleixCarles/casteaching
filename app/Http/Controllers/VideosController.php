7c23f79514f0c234bef6b495f67ee22f7f36181e
AleixCarles <aleixcarles@iesebre.com>
2023-05-06 18:38:25 +0200

    Video 142 Navegació utilitzant series, Finalitzat i 143 Millores a la UI, testos regressius i solucions vídeo 142,Finalitzat

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Video;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Tests\Feature\Videos\VideoTest;
 
 class VideosController extends Controller
@@ -14,10 +15,22 @@ class VideosController extends Controller
     }
     public function show($id)
     {
+        $video = Video::findOrFail($id);
+        if ($video->published_at === null) {
+            if (!optional(Auth::user())->can('videos_manage_show')) {
+                if ($video->user_id === null){
+                    abort(404);
+                }
+                if (!($video->user_id == optional(Auth::user())->id)) {
+                    abort (404);
+                }
+            }
+        }
 
-    return view('videos.show',[
-        'video'=>Video::findOrFail($id)
-    ]);
+        return view(count($videos_series = collect($video->serie?->videos)) > 0 ? 'videos.show' : 'videos.show_without_series_navigation',[
+            'video' => $video,
+            'videos_series' => $videos_series
+        ]);
 
     }
 }
